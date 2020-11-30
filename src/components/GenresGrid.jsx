ce2c275953edb8b4e1f8e65b82f2b2d154dd7e0b
Jerome Nicco <jeromenicco@MacBook-Pro-de-Jerome.local>
2020-11-30 16:13:38 +0100

    first commit

---
+++ src/components/GenresGrid.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import GridItem from './GridItem';
+import './GenresGrid.css';
+
+const GenresGrid = ({ filterGenres }) => {
+  return (
+    <div className="grid-container">
+      {filterGenres.map((genre, i) => {
+        return (
+          <GridItem key={i} {...genre} />
+        );
+      })}
+    </div>
+  );
+};
+
+GenresGrid.propTypes = {
+  token: PropTypes.string.isRequired,
+  filterGenres: PropTypes.string.isRequired
+};
+
+export default GenresGrid;
---
+++ src/components/GridItem.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import './GridItem.css';
+
+const GridItem = ({ ...genre }) => {
+  const [isShown, setIsShown] = useState(false);
+
+  const history = useHistory();
+
+  const handleClick = (id) => {
+    history.push(`/playlist/${id}`);
+  };
+
+  return (
+    <div
+      className='grid-item'
+      onMouseEnter={() => setIsShown(true)}
+      onMouseLeave={() => setIsShown(false)}
+      onClick={() => handleClick(genre.id)}>
+      <img src={(genre.icons[0].url)} alt="picture" className={isShown && 'hoveredImage'} />
+      {isShown && <p className='hoveredItem'>{genre.name.replace(/[&]/g, 'n').replace(/[/-]/g, ' ')}</p>}
+      <h4 className='showOnMobile'>{genre.id}</h4>
+    </div>
+  );
+};
+
+GridItem.propTypes = {
+  filterGenres: PropTypes.string.isRequired
+};
+
+export default GridItem;
